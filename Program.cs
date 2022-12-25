782f14e2eaa6b1084ca341bd79822050ef66b534
AbdelrahmanElmongy1 <121382605+AbdelrahmanElmongy1@users.noreply.github.com>
2022-12-25 13:59:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+//
+            Console.WriteLine("n1");
+            int S = int.Parse(Console.ReadLine());   
+            Console.WriteLine("n2");
+            int E = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for(int x=S;x<=E;x++)
+            {
+                sum = 0;
+                for (int i = 1; i <= x / 2; i++)
+                {
+                    if (x % i == 0)
+                        sum += i;              
+                }
+                if (sum == x)
+                    Console.WriteLine( x + "  Is A Perfect Number");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
